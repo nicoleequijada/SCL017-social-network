0d81324f2c2929cea5f75af19a3629a1067fe41d
NatalyFarias <nataly.fariasg@gmail.com>
2021-08-11 19:46:17 -0400

    modificaciones en los test

--- test/authetication.spec.js
+++ test/authetication.spec.js
@@ -1,6 +1,6 @@
 // importamos la funcion que vamos a testear
 import { signInWithPassword } from '../src/lib/auth/authetication';
-jest.mock("./authetication")
+jest.mock("../src/lib/auth/__mocks__/authetication")
 
 describe('signInWithPassword', () => {
   it('debería ser una función', () => {
@@ -8,7 +8,7 @@ describe('signInWithPassword', () => {
   });
 
   it('debería logearse y que el usuario verifico su email', () => {
-    signInWithPassword('nataly.fariasg@gmail.com', '12345678').then(res => {
+    signInWithPassword('nataly.fariasg@gmail.com', '12345679').then(res => {
       // const responseSign = JSON.parse(JSON.stringify(res));
       expect(res.user.emailVerified).toBe(true)
     })
